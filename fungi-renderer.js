73b19a63670f4d546276f11643ffbd030e3e5bb8
FeatheredSnek <dorian.maczka@gmail.com>
2021-06-27 17:23:37 +0200

    Renderer comments

--- fungi-renderer.js
+++ fungi-renderer.js
@@ -1,3 +1,16 @@
+/*
+HTML DOM renderer object. Keeps track of the game and
+the DOM Nodes provided by the caller. Its job is to display and animate elements
+based on the game's data.
+It is by no means abstracted - it's quite tightly one-way coupled with the
+Game class - it's build on top of the Game as its visual extension.
+The renderer instance provides no responses or information to the external
+controller that operates it - it just modifies the elements that were provided.
+It would be possible to design it this way - so, for example, it would return
+timings or even promises when animation is playing. However, I decided that the
+renreder should be oblivious of that and resort to displaying.
+*/
+
 class Renderer {
   constructor (
     game,
@@ -12,7 +25,7 @@ class Renderer {
     this.timeCounterDOMElement = timeCounterDOMElement
     this.overlayDOMElement = overlayDOMElement
   }
-
+  // Timings are used internally or externally by the controller.
   static animationTimes = {
     slotAction: 700,
     slotWarning: 500,
@@ -39,8 +52,12 @@ class Renderer {
     )
   }
 
+  /*
+  Functions below deal with slots, both in the inventory or on the board.
+  All of them depend primarily on the renderSlot function.
+  */
   createSlot (slotData, slotId, containerDOMElement) {
-    // initialize empty slot
+    // Initialize empty slot
     let slotDOMElement = document.createElement('div')
     slotDOMElement.id = slotId
     slotDOMElement.classList.add('slot')
@@ -49,14 +66,18 @@ class Renderer {
     slotDOMElement.setAttribute('type', 'null')
     slotDOMElement.setAttribute('stage', 'null')
     containerDOMElement.append(slotDOMElement)
-    // generate and add grassy background for board slots
+    // Generate and add grassy background for board slots
     if (containerDOMElement == this.boardDOMElement) {
       slotDOMElement.setAttribute('style', this.getGrassBackgroundRule())
     }
-    // render its contents
+    // Render the slot's contents
     this.renderSlot(slotData, slotDOMElement, true)
   }
 
+  /*
+  Update functions may be called by the externall controller when there's
+  a need to display the changes within the game.
+  */
   updateBoard () {
     for (let slotDOMElement of this.boardDOMElement.children) {
       this.updateBoardSlot(slotDOMElement)
@@ -79,14 +100,24 @@ class Renderer {
     this.renderSlot(slotData, slotDOMElement, true)
   }
 
+  /*
+  renderSlot is the most important functionality of this renderer - based
+  on the slotData provided it changes the visuals within slotDOMElement
+  by operating on it's classList.
+  The value of the 'animated' argument is not used.
+  The function compares the properties of slotData - the current state of the
+  slot provided by the game - with the properties of slotDOMElement - previously
+  rendered state of the slot. Based on that it decides what to display and how
+  to animate the transition. There are 5 possible cases:
+  */
   renderSlot (slotData, slotDOMElement, animated) {
     let slotImage = slotDOMElement.children[0]
-    // slot contents do not change => do nothing
+    // 1) Contents of a full slot do not change -> do nothing
     if (slotDOMElement.getAttribute('stage') == slotData.stage && slotDOMElement.getAttribute('type') == slotData.type) {
       return
     }
-    // empty into full => grow
-    // full into full => grow
+    // 2) Empty slot becomes full -> animate growth
+    // 3) Full slot stays full -> animate growth
     else if (slotData.type != null && slotData.stage != null) {
       slotDOMElement.setAttribute('type', slotData.type)
       slotDOMElement.setAttribute('stage', slotData.stage)
@@ -97,7 +128,7 @@ class Renderer {
       slotImage.alt = this.getSlotDescription(slotData.type, slotData.stage)
       setTimeout(() => slotImage.classList.remove('grow'), Renderer.animationTimes.slotAction)
     }
-    // full into empty => disappear
+    // Full slot becomes empty -> animate disappearance
     else if (slotData.type == null && slotData.stage == null && slotImage.className == 'full') {
       slotDOMElement.setAttribute('type', slotData.type)
       slotDOMElement.setAttribute('stage', slotData.stage)
@@ -108,12 +139,15 @@ class Renderer {
         slotImage.className = 'empty'
       }, Renderer.animationTimes.slotAction)
     }
-    // empty into empty => do nothing
+    // Empty slot stays empty -> do nothing
     else if (slotData.type == null && slotData.stage == null && slotImage.className == 'empty') {
       return
     }
   }
 
+  /*
+  Wiggling animation provides feedback when an illegal action is attempted
+  */
   wiggleSlot (slotDOMElement) {
     let slotImage = slotDOMElement.children[0]
     slotImage.classList.add('wiggle')
@@ -126,6 +160,9 @@ class Renderer {
     }
   }
 
+  /*
+  Two helper functions that transform raw slot data into appropriate strings
+  */
   getSlotImageSource (type, stage) {
     return `images/fungi/${type}-${stage}.svg`
   }
@@ -134,6 +171,14 @@ class Renderer {
     return `${type[0].toUpperCase() + type.slice(1)}, growth stage ${stage}`
   }
 
+  /*
+  Updating gold & time - 'score' - counters comes with a simple animation.
+  As above, the renderer compares the actual data received from the game
+  and the data that is currently displayed and acts accordingly.
+  Updating gold counter is more complex as it involves (1) two animations - one
+  for a gain and one for a loss of gold - and (2) visual indicator of
+  a negative gold value.
+  */
   updateGoldCounter () {
     let currentGold = this.goldCounterDOMElement.textContent
     let newGold = this.game.gold
@@ -161,12 +206,21 @@ class Renderer {
     this.timeCounterDOMElement.textContent = this.game.time
   }
 
+  /*
+  Button's visual feedback, can be called with additional 'small' argument for
+  a very little popin (indicating that an action is invalid)
+  */
   buttonPopIn (buttonDOMElement, small) {
     let classSizeName = small ? 'popin-small' : 'popin'
     buttonDOMElement.classList.add(classSizeName)
     setTimeout(() => buttonDOMElement.classList.remove(classSizeName), Renderer.animationTimes.buttonAction)
   }
 
+  /*
+  Overlay is also controlled by manipulating classList - css classes themselves
+  contain all the required animation data. Current overlay serves only
+  one purpose - to inform the player of the GAME OVER state.
+  */
   showOverlay () {
     this.overlayDOMElement.classList.remove('hidden')
     setTimeout(() => this.overlayDOMElement.classList.remove('invisible'), 50)
@@ -184,6 +238,9 @@ class Renderer {
     this.showOverlay()
   }
 
+  /*
+  Arbitrary timing values below
+  */
   restart () {
     setTimeout(() => {
         this.updateBoard();
@@ -195,27 +252,43 @@ class Renderer {
     setTimeout(() => this.hideOverlay(), 100)
   }
 
+  /*
+  This rather convoluted function returns a string that is used to style
+  a board slot with some randomly generated grass background. The class list
+  cannot be used here, so we style each element directly.
+  */
   getGrassBackgroundRule () {
+    // Multiple backgrounds are used, one for each blade of grass
     let backgroundRule = 'background-image: '
     let backgroundPositionRule = 'background-position: '
     let backgroundRepeatRule = 'background-repeat: '
     let backgroundSizeRule = 'background-size: '
+    // Seed variable is reused with new values to increase readability a bit
     let seed = Math.random()
+    // Background can contain 2 or 3 blades of grass
     let blades = seed > 0.5 ? 2 : 3
+    // Each blade...
     for (let i = 0; i < blades; i++) {
+      // ...may be light or dark colored...
       seed = Math.random()
       let color = seed > 0.5 ? 'dark' : 'light'
+      // ...big, small or medium sized.
       seed = Math.random()
       let size = seed < 0.33 ? 'small' : seed > 0.33 && seed < 0.66 ? 'medium' : 'big'
+      // Color and size determine which svg image will be used
       let url = `url(images/ui/grass-${color}-${size}.svg)`
+      // Its position is randomized on both axes
       let positionX = Math.floor(Math.random() * (90 - 10 + 1)) + 10
       let positionY = Math.floor(Math.random() * (90 - 10 + 1)) + 10
+      // For the last blade the rule string must end with ; for others with ,
       let ending = i == blades - 1 ? ';' : ', '
+      // Information about the blade is converted and added to styling strings
       backgroundRule += `${url}${ending}`
       backgroundPositionRule += `${positionX}% ${positionY}%${ending}`
       backgroundRepeatRule += `no-repeat${ending}`
       backgroundSizeRule += `15%${ending}`
     }
+    // Then all rules describing all the blades are returned and used by the slot generator
     return backgroundRule + backgroundPositionRule + backgroundRepeatRule + backgroundSizeRule
   }
 
