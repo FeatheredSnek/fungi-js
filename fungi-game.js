205b2f698ce51d0db0a01ab137f3c18a8cbab34e
FeatheredSnek <dorian.maczka@gmail.com>
2021-06-27 16:06:19 +0200

    Game class comments

--- fungi-game.js
+++ fungi-game.js
@@ -1,5 +1,18 @@
+/*
+This main class for the game object contains the board,
+the inventory and the gold & time values.
+It controls them via action functions (described below), which,
+as the name suggests, dispatch actions to board or inventory elements.
+Note that this class does not monitor the 'state' of a game i.e.
+if a game is won or lost. Instead it returns action responses,
+which then can be handled by the external controller displaying
+fail/success states. This gives some more flexibility and
+allows for easier testing.
+*/
+
 class Game {
 
+  // This static enum style is used in Godot - you can easily notice allcaps
   static actionResponses = {
     'CONTINUE': 0,
     'SLOT_NOT_PICKABLE': 1,
@@ -9,7 +22,7 @@ class Game {
   }
 
   constructor (boardSize, inventorySize, settings) {
-
+    // individually copy settings' values to this Game instance for easy access
     this.mushroomTable = settings.mushrooms
     this.advanceCost = settings.advanceCost
     this.pickupCost = settings.pickupCost
@@ -19,22 +32,34 @@ class Game {
     this.sameTypeBonusMultiplier = settings.sameTypeBonusMultiplier
     this.sameStageBonusMultiplier = settings.sameStageBonusMultiplier
     this.tricolorBonus = settings.tricolorBonus
-
+    /*
+    The probability P of an event happening is often expressed as x/1
+    however, when adjusting the growth settings or adding new fungi
+    it would be tedious to manually calculate the probabilites so they sum to 1.
+    The game does it by calculating probabilites as p(F1)/p(F1)+p(F2)+...+p(Fn).
+    */
     this.frequencySum = settings.emptySlotFrequency
     settings.mushrooms.forEach(mushroom => this.frequencySum += mushroom.frequency)
+    /*
+    The probabilites of certain fungi appearing on board are stored in an array
+    of tresholds. When a number between 0 and frequencySum is given we can then
+    check into which treshold it falls - and the index of this treshold will be
+    the index of a fungus that should be generated.
+    */
     this.populationTresholds = []
     let currentTreshold = 0
     for (let mushroom of settings.mushrooms) {
       currentTreshold += mushroom.frequency
       this.populationTresholds.push(currentTreshold)
     }
+    // This 0-1 number tells us the chance of an empty slot being repopulated.
     this.repopulationFactor = settings.repopulationFactor
-
+    // 'Score' values, starting values are kept and used for resetting the game.
     this.startingGold = settings.startingGold
     this.gold = settings.startingGold
     this.maxGoldCap = settings.maxGoldCap
     this.time = 0
-
+    // Generate inventory and an empty board.
     this.inventory = new Inventory(inventorySize)
     this.board = []
     for (let i = 0; i < boardSize; i++) {
@@ -42,13 +67,18 @@ class Game {
     }
   }
 
+  /*
+  These three small helper functions below are used by the 'action functions'
+  to control the board. You may easily turn the board from an array-type property
+  of the Game to a separate Board object - but it would be a glorified array
+  with little extra functionality so I didn't bother.
+  */
   clearBoard () {
     this.board.forEach(slot => slot.empty)
   }
 
   advanceBoard () {
     this.board.forEach(slot => slot.advance(this.mushroomTable, this.populationTresholds, this.frequencySum, this.repopulationFactor))
-    this.previewBoard()
   }
 
   repopulateBoard () {
@@ -56,6 +86,14 @@ class Game {
     this.advanceBoard()
   }
 
+  /*
+  Below are the 'action functions' - functions through which an external
+  controller (user interface, machine learning program etc.) controls the game.
+  Each action function returns an 'action response' that can be read by the
+  external controller. In order to facilitate communication between the game
+  and the controller the responses are stored as static enum values in the
+  Game class (see above, before the constructor).
+  */
   actionAdvance () {
     this.advanceBoard()
     this.time += 1
@@ -94,6 +132,24 @@ class Game {
     }
   }
 
+  actionStart () {
+    this.gold = this.startingGold
+    this.time = 0
+    this.inventory.empty()
+    this.repopulateBoard()
+  }
+
+  /*
+  This helper function tells the external controller whether the game is
+  'continuable' or 'lost'. It checks four conditions:
+  1) can the inventory be sold for a value that results in positive gold,
+  2) can the board be advanced,
+  3) can anything be picked up from the board.
+  It returns NO_LEGAL_MOVES when no condition is met - i.e. when
+  absolutely nothing can be done to gain gold in the next move.
+  It is up to the external controller whether this results in an info,
+  a warning or a gameover-type halt.
+  */
   checkLegalMoves () {
     let inventoryContentCondition = this.inventory.isFull()
     let inventoryValueCondition = this.inventory.getTotalValue(
@@ -119,17 +175,10 @@ class Game {
     }
   }
 
-  start () {
-    this.gold = this.startingGold
-    this.time = 0
-    this.inventory.empty()
-    this.repopulateBoard()
-  }
-
+  // Two helper functions for debug purposes
   previewInventory() {
     console.table(this.inventory)
   }
-
   previewBoard () {
     console.table(this.board)
   }
